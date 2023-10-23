42bde7b69432ae608a8ecb3196f6fde92c240fb8
Douglas <115107776+Bastouwln@users.noreply.github.com>
2023-10-23 12:09:52 -0300

    Add files via upload

---
+++ siteCursoTecnico-main/CursoTécnico/js/index.js
@@ -0,0 +1,11 @@
+function paranaRemove() {
+            var parana = document.getElementById("parana");
+           if(parana){
+            parana.remove(parana);
+           }
+}
+function hoverIn(){
+    a = document.getElementById = "imgwoman";
+    
+}
+paranaRemove();
